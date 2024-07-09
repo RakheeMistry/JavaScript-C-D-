ddd8110edaf98c305abfb43c55ad7c808769fa23
RakheeMistry <95229428+RakheeMistry@users.noreply.github.com>
2024-07-09 14:19:59 +0000

    conversion done

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -7,14 +7,14 @@
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber);
 
-let score1 = null;
-// let score1 = undefined;
-console.log(typeof score1);
-console.log(typeof (score1));
+// let score1 = null;
+// // let score1 = undefined;
+// console.log(typeof score1);
+// console.log(typeof (score1));
 
-let valueInNumber1 = Number(score1)
-console.log(typeof valueInNumber1);
-console.log(valueInNumber1);
+// let valueInNumber1 = Number(score1)
+// console.log(typeof valueInNumber1);
+// console.log(valueInNumber1);
 
 //"33" => 33
 //"33abc" => NaN
@@ -28,8 +28,39 @@ console.log(valueInNumber1);
 //"" => false
 //"hitesh" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+//=================== Operations ======================
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello";
+let str2 = " Rakhee";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+//++gameCounter;
+gameCounter++;
+console.log(gameCounter);
