ac720a6296bea36194254e0c8beef8b640e5bf18
RakheeMistry <95229428+RakheeMistry@users.noreply.github.com>
2024-07-09 13:03:05 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+// let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let score1 = null;
+// let score1 = undefined;
+console.log(typeof score1);
+console.log(typeof (score1));
+
+let valueInNumber1 = Number(score1)
+console.log(typeof valueInNumber1);
+console.log(valueInNumber1);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//"" => false
+//"hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
