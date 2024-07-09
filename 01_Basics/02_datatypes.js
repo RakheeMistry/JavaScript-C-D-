d5413bc706d0979ad62c1cd94f922295d880943d
RakheeMistry <95229428+RakheeMistry@users.noreply.github.com>
2024-07-09 07:36:14 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3 
+    + 3
+
+) // code readability should be high
+
+console.log("Rakhee");
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigInt
+// boolean => true/false
+// string => ""
+// null => standalone value
+// undefined 
+// symbol => unique
+
+
+// object
+console.log(typeof "hi");
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
