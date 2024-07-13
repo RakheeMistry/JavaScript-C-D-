c2f7c6765ba642024604b1234fafd7553551b40f
RakheeMistry <95229428+RakheeMistry@users.noreply.github.com>
2024-07-13 16:55:59 +0000

    control Flow in-process

---
+++ 01_Basics/15_controlFlow.js
@@ -0,0 +1,23 @@
+// if
+// const isUserloggedaIn = true
+
+// if(isUserloggedaIn){
+
+// }
+
+const temperature = 41
+if( temperature < 50)
+{
+    console.log("less than 50")
+}
+else {
+    console.log("temperature is greater than 50");
+}
+
+
+// if(2 == '2'){
+//     console.log("executed");
+// }
+
+// <, >, <=, >=, ==, !=, ===
+
