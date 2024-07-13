70f0393ed5c92d05a3ca2bac8800d2aa06d73a8e
RakheeMistry <95229428+RakheeMistry@users.noreply.github.com>
2024-07-13 02:36:17 +0000

    scopes done

---
+++ 01_Basics/12_scopes.js
@@ -0,0 +1,50 @@
+// var c = 300
+// let a = 300
+// if(true){
+//     let a = 10
+//     const b = 20
+//     var c = 30
+//     console.log("Inner:", a)
+// }
+
+// console.log(a)
+// // console.log(b)
+// console.log(c)
+
+// function one(){
+//     const username = "rakhee"
+
+//     function two(){
+//       const website = 'youtube'
+//       console.log(username);  
+//     }
+//     // console.log(website);
+//     two();
+// }
+// one()
+
+
+
+// if(true) {
+//     const username = "rakhee"
+//     if(username === "rakhee")
+//     {
+//         const website = "youtube"
+//         console.log(username + website)
+//     }
+//      console.log(website)
+// }
+//  console.log(username)
+
+
+// ***************Interesting***************
+
+console.log(addone(5))
+function addone(value){
+    return value + 1
+}
+
+//console.log(addTwo(5))
+const addTwo = function(num){
+    return num + 2
+}
