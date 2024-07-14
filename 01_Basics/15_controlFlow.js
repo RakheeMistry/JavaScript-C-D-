d439a9a979fb71a12cdd21cca3ef066b69d9831a
RakheeMistry <95229428+RakheeMistry@users.noreply.github.com>
2024-07-14 05:50:19 +0000

    controlFlow done

--- 01_Basics/15_controlFlow.js
+++ 01_Basics/15_controlFlow.js
@@ -1,3 +1,4 @@
+// <, >, <=, >=, ==, !=, ===
 // if
 // const isUserloggedaIn = true
 
@@ -5,19 +6,54 @@
 
 // }
 
-const temperature = 41
-if( temperature < 50)
-{
-    console.log("less than 50")
-}
-else {
-    console.log("temperature is greater than 50");
-}
+// const temperature = 41
+// if( temperature < 50)
+// {
+//     console.log("less than 50")
+// }
+// else {
+//     console.log("temperature is greater than 50");
+// }
 
 
 // if(2 == '2'){
 //     console.log("executed");
 // }
 
-// <, >, <=, >=, ==, !=, ===
+// if (2 != 3) {
+//     console.log("executed");
+// }
+
+// const score = 200;
+// if(score > 100){
+//     const power = "fly";
+//     console.log(`User power: ${power}`);
+// }
+//console.log(`User power: ${power}`);
+
+const balance = 1000
+// if(balance > 500) console.log("test"), console.log("Test2"); // not recommended (violate readability)
+
+// if(balance < 500)
+// {
+//   console.log("less than");  
+// } else if(balance < 750)
+// {
+//   console.log("less than 750");
+// } else {
+//   console.log("less than 1200");
+// }
+
+const userLoggedIn = true;
+const debitCard = true;
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+
+// if(userLoggedIn && debitCard && 2==3) {
+//     console.log("Allow to buy course");
+// }
+
+// if(loggedInFromGoogle || loggedInFromEmail) {
+//     console.log("User logged in");
+// }
 
