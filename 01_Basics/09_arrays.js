b61458fe4cd0155e9ce72c367005d6fe36da43eb
Rakhee Mistry <rsmistry05061996@gmail.com>
2024-08-09 16:04:45 +0530

    modified array file

--- 01_Basics/09_arrays.js
+++ 01_Basics/09_arrays.js
@@ -22,11 +22,50 @@ const newArr = myArr.join();
 // console.log(newArr);
 
 // slice, splice
-console.log("A ", myArr);
+// console.log("A ", myArr);
 const myn1 = myArr.slice(1,3);
-console.log(myn1);
+// console.log(myn1);
 
-console.log("B ", myArr);
+// console.log("B ", myArr);
 const myn2 = myArr.splice(1,3);
-console.log("C ", myArr);
-console.log(myn2);
+// console.log("C ", myArr);
+// console.log(myn2);
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+const marvel_heros = ["thor", "ironman", "spiderman"];
+const dc_heros = ["superman", "flash", "batman"];
+
+//marvel_heros.push(dc_heros);
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+const allHeros = marvel_heros.concat(dc_heros);
+//console.log(allHeros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros];
+//console.log(all_new_heros);
+
+const another_array = [1, 3, 4, [4, 6, 7], 8, [1, 5, [4, 7]]];
+
+const real_another_array = another_array.flat(Infinity);
+console.log(real_another_array);
+
+console.log(Array.isArray("Hitesh"));
+console.log(Array.from("Hitesh"));
+console.log(Array.from({name: "hitesh"})) // interesting
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3));
+
+
+
+
+
+
+
+
+
