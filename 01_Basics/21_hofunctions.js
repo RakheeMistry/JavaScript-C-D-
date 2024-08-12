d45882e114141e5db32265320166280b0ac6fa52
Rakhee Mistry <rsmistry05061996@gmail.com>
2024-08-12 18:39:00 +0530

    higher order functions file done

---
+++ 01_Basics/21_hofunctions.js
@@ -0,0 +1,128 @@
+//----------------------------- forEach------------------------------------
+const coding = ["js", "ruby", "java", "python", "cpp"];
+
+// coding.forEach(function name(item){
+//     console.log(item);
+// })
+
+// coding.forEach( (val) => {
+//     console.log(val);
+// })
+
+// function printMe(item) {
+//     console.log(item);
+// }
+// coding.forEach(printMe);
+
+// coding.forEach((item, index, arr) => {
+//     console.log(item, index, arr);
+// })
+
+const myCoding = [
+    {
+        languageName: "Javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "Java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "Python",
+        languageFileName: "py"
+    },
+    {
+        languageName: "C",
+        languageFileName: "c"
+    }
+];
+
+myCoding.forEach((item)=>{
+    //console.log(item.languageName);
+});
+
+//--------------------------filter----------------------------
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const newNums = myNums.filter((num) => num > 4 );
+// console.log(newNums);
+
+// same example as above with forEach
+const newNums = [];
+myNums.forEach((num) => { 
+    if(num > 4){
+        newNums.push(num);
+    }});
+//console.log(newNums);
+
+const books = [
+    { title:'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title:'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title:'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title:'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title:'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title:'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title:'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title:'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title:'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+];
+
+// const userBooks = books.filter((bk) => bk.genre === 'Non-Fiction' );
+// console.log(userBooks);
+
+// const userBooks1 = books.filter((bk) => {return  bk.publish >= 2000} );
+// console.log(userBooks1);
+
+// const userBooks2 = books.filter((bk) => {return  bk.publish >= 1995 && bk.genre === "History"} );
+// console.log(userBooks2);
+
+//-------------------------------map-------------------------------
+
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const no = myNumbers.map((num) => num + 10);
+//console.log(no);
+
+// same above example with forEach
+const w = [];
+myNumbers.forEach((num)=>{w.push(num + 10)});
+//console.log(w);
+
+// chaining of higher order functions
+const chain = myNumbers.map((num) => num * 10)
+                    .map((num) => num + 1)
+                    .filter((num) => num >= 40);
+//console.log(chain);
+
+//---------------------------------reduce----------------------------
+
+const myNum = [1, 2, 3];
+// const myTotal = myNum.reduce((acc, currval) => {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval;
+// }, 0);
+
+const myTotal = myNum.reduce((acc, currval) =>  acc + currval, 0); // simple way or shortcut to represent reduce
+
+console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+];
+
+const priceToPay = shoppingCart.reduce((acc,item) => acc + item.price , 0);
+console.log(priceToPay);
